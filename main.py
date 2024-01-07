5f33bea919371db38dd99b5698a103d17bc0d0c0
le1uk <alexey.osipychev@yandex.ru>
2024-01-07 01:47:16 +0100

    added new endpoint /api/top_250

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from parser import get_top
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     return '<h1>Привет, Стас!</h1>'
+
+
+@app.route('/api/top_250')
+def top_250():
+    films = get_top()
+    return f'<h1>ТОП 250: {films}</h1>'
+
+
+@app.route('/api/<get_film>')
+def get_film(get_film: str) -> str:
+    return f'<h1>Фильм который вы ищите: {get_film}</h1>'
 
 
 if __name__ == '__main__':
